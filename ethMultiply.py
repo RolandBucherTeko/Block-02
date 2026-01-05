e69a10688b8eb550c664e3b9a37f9af49d4a40e8
Roland Bucher <roland.bucher@edu.teko.ch>
2026-01-05 22:03:23 +0100

    korrigiert ganzzahlige Division

--- ethMultiply.py
+++ ethMultiply.py
@@ -4,6 +4,6 @@ def ethiopian_multiply(a, b):
     while a > 0:
         if a % 2 != 0:
             result += b
-        a = a // 2      
+        a = a // 2        #ganzzahlige Division 
         b = b * 2
     return result
