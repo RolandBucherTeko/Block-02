3503454270c623dfcfbc4a32285048613b3c0879
Roland Bucher <roland.bucher@edu.teko.ch>
2026-01-05 21:44:55 +0100

    Optimiert

--- ethMultiply.py
+++ ethMultiply.py
@@ -4,6 +4,6 @@ def ethiopian_multiply(a, b):
     while a > 0:
         if a % 2 != 0:
             result += b
-        a = a // 2      # GANZZAHLIGE Division
+        a = a // 2      
         b = b * 2
     return result
